f896e2cc6e290f33bde3d04dfc28242a281531fd
namhla1221 <namhlamthi21@gmail.com>
2020-11-20 10:38:07 +0200

    new changes

--- test/waiter-test.js
+++ test/waiter-test.js
@@ -18,22 +18,6 @@ const pool = new Pool({
 const Waiter = require('../waiter');
 let waiter = Waiter(pool);
 
-describe('The weekdays function',  function (){
-  beforeEach(async  function () {
-    await pool.query('DELETE FROM dayShifts');
-    await pool.query('DELETE FROM waiterDB');
-    await pool.query('DELETE FROM weekdays');
-  });
-  it('should add all the weekdays', async function (){
-    await waiter.weekDays();
-    assert.deepEqual(await waiter.getdays(), [
-      { dayname: 'Sunday'},{dayname: 'Monday'},
-      { dayname: 'Tuesday'},{dayname: 'Wednesday'},
-      { dayname: 'Thursday'},{dayname: 'Friday'},{dayname: 'Saturday'}
-    ]);
-  });
-});
-
 describe('The Add function  add waiters', function () {
   beforeEach(async function() {
     await pool.query('DELETE FROM dayShifts');
@@ -46,53 +30,6 @@ describe('The Add function  add waiters', function () {
     assert.equal(storedWaiter, true);
   });
 });
-
-describe('finding user fullname', function () {
-  it('should return with vailid fullname',async function () {
-    await waiter.add_waiter("Vee", "Vuyokazi", "waiter");
-    let fullname = await waiter.getUsername("Vee");
-    assert.equal(fullname,'Vuyokazi');
-  })
-  })
-
-
-describe('find a Waiter or Admin', function () {
-
-it('should return  waiter',async function () {
-  await waiter.add_waiter("Vee", "Vuyokazi", "waiter");
-  let loginHas = await waiter.foundUser("Vee","waiter");
-  assert.equal(loginHas , true);
-})
-
-it('should return  admin',async function () {
-  await waiter.add_waiter("Sinono", "Sinovuyo", "admin");
-  let loginHas = await waiter.foundUser("Sinono","admin");
-  assert.equal(loginHas,'admin');
-})
-})
-
-
-
-describe('waiter should Select a shift', function () {
-  beforeEach(async function () {
-    await pool.query('DELETE FROM dayShifts');
-    await pool.query('DELETE FROM waiterDB');
-    await pool.query('DELETE FROM weekdays');
-  });
-  it('should allow user to add shift', async function () {
-    await waiter.weekDays();
-    await waiter.add_waiter("Vee", "Vuyokazi", "waiter");
-    await waiter.add_waiter("Njunju", "Nwabisa", "waiter");
-    let shift = {
-      username: 'Vee',
-      days: ["Monday", "Thursday", "Wednesday"]
-    }
-    let select_shift = await waiter.dayShift(shift);
-    assert.deepEqual(select_shift, true);
-  })
-});
-
-
 
 describe('Select a shift', function () {
   beforeEach(async function () {
@@ -114,6 +51,63 @@ describe('Select a shift', function () {
     }]);
   })
 });
+
+describe('The weekdays function',  function (){
+  beforeEach(async  function () {
+    await pool.query('DELETE FROM dayShifts');
+    await pool.query('DELETE FROM waiterDB');
+    await pool.query('DELETE FROM weekdays');
+  });
+  it('should add all the weekdays', async function (){
+    await waiter.weekDays();
+    assert.deepEqual(await waiter.getdays(), [
+      { dayname: 'Sunday'},{dayname: 'Monday'},
+      { dayname: 'Tuesday'},{dayname: 'Wednesday'},
+      { dayname: 'Thursday'},{dayname: 'Friday'},{dayname: 'Saturday'}
+    ]);
+  });
+});
+
+
+describe('finding user fullname', function () {
+  it('should return with vailid fullname',async function () {
+    await waiter.add_waiter("Vee", "Vuyokazi", "waiter");
+    let fullname = await waiter.getUsername("Vee");
+    assert.equal(fullname,'Vuyokazi');
+  })
+  })
+
+
+describe('find a Waiter ', function () {
+
+it('should return  waiter',async function () {
+  await waiter.add_waiter("Vee", "Vuyokazi", "waiter");
+  let loginHas = await waiter.foundUser("Vee","waiter");
+  assert.equal(loginHas , true);
+})
+});
+
+
+describe('waiter should Select a shift', function () {
+  beforeEach(async function () {
+    await pool.query('DELETE FROM dayShifts');
+    await pool.query('DELETE FROM waiterDB');
+    await pool.query('DELETE FROM weekdays');
+  });
+  
+  it('should allow user to add shift', async function () {
+    await waiter.weekDays();
+    await waiter.add_waiter("Vee", "Vuyokazi", "waiter");
+    await waiter.add_waiter("Njunju", "Nwabisa", "waiter");
+    let shift = {
+      username: 'Vee',
+      days: ["Monday", "Thursday", "Wednesday"]
+    }
+    let select_shift = await waiter.dayShift(shift);
+    assert.deepEqual(select_shift, true);
+  })
+});
+
 
 describe('Get all stored shifts', function () {
   it('should return a list of stored shifts', async function () {
